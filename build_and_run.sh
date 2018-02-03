e03894f415676fb91872c5c5d1b6cb13cb60db03
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-03 07:49:23 -0500

    use $HOME in build and run scripts

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,8 +4,8 @@ sudo docker build -t ec . && \
 sudo docker run \
 	--rm \
 	-it \
-	-v /home/user/.electron-cash/:/home/user/.electron-cash/ \
-	-v /home/user/ec-backup/:/home/user/ec-backup/ \
+	-v $HOME/.electron-cash/:/home/user/.electron-cash/ \
+	-v $HOME/ec-backup/:/home/user/ec-backup/ \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY \
 	--name ec \
