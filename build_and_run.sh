d43b5c52d033e5e615b2b85f6b30297fde280c52
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-02 21:50:23 -0500

    remove xpra

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,4 @@
 sudo docker build -t ec . && \
-#sudo docker run --name ec --rm -it -u user -v /home/user/.electron-cash/:/home/user/.electron-cash/ -p 127.0.0.1:10000:10000 ec xpra start :100 --compressors=lz4 --start-child=/usr/local/bin/electron-cash --bind-tcp=0.0.0.0:10000 --no-daemon --no-mdns --no-notifications --no-pulseaudio --exit-with-children
 
 #	-v $HOME/.Xauthority:/$HOME/.Xauthority \
 sudo docker run \
