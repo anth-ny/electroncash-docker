a60dcc54554c35fb1693202d33cdfb7ae88e2933
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-03 08:46:55 -0500

    add test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#	-v $HOME/.Xauthority:/$HOME/.Xauthority \
+sudo docker run \
+	--rm \
+	-it \
+	-v $HOME/.electron-cash/:/home/user/.electron-cash/ \
+	-v $HOME/ec-backup/:/home/user/ec-backup/ \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e DISPLAY \
+	--init \
+	--name ec \
+	ec
