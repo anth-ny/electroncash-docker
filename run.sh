cb2129c10f2a728e9a6a2bff07e6046f71a15ea2
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-02 22:54:21 -0500

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#	-v $HOME/.Xauthority:/$HOME/.Xauthority \
+sudo docker run \
+	--rm \
+	-it \
+	-v /home/user/.electron-cash/:/home/user/.electron-cash/ \
+	-v /home/user/ec-backup/:/home/user/ec-backup/ \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e DISPLAY \
+	--name ec \
+	ajdjd/electroncash
