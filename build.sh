a3c4e8951ba18051d73bfacdcc47a9a6d9469a6a
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-11 09:57:13 -0500

    remove old docker-machine-id-setup references

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 cd shared
-#cd docker-machine-id-setup && sudo docker build -t docker-machine-id-setup .
 cd ../shared && sudo docker build -t electroncash-shared .
-#sudo docker tag docker-machine-id-setup ajdjd/docker-machine-id-setup
 sudo docker tag electroncash-shared ajdjd/electroncash:shared
 cd ../latest && sudo docker build -t ec .
 sudo docker rmi ajdjd/electroncash:shared
-#sudo docker rmi ajdjd/docker-machine-id-setup
 sudo docker tag ec electroncash
