ef0eaaa1f5ecfcb66102ffb6d65061a04631480b
Anthony DiPierro <anth-ny@users.noreply.github.com>
2018-02-11 12:25:55 -0500

    die on single error

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cd shared
-cd ../shared && sudo docker build -t electroncash-shared .
-sudo docker tag electroncash-shared ajdjd/electroncash:shared
-cd ../latest && sudo docker build -t ec .
-cd ../dev && sudo docker build -t ecdev .
+cd shared && \
+cd ../shared && sudo docker build -t electroncash-shared . && \
+sudo docker tag electroncash-shared ajdjd/electroncash:shared && \
+cd ../latest && sudo docker build -t ec . && \
+cd ../dev && sudo docker build -t ecdev . && \
 sudo docker rmi ajdjd/electroncash:shared
